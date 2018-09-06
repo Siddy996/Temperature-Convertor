013db8ffaa5cf943a01ea7028fe3a24c11950002
Siddy996 <30317417+Siddy996@users.noreply.github.com>
2018-09-06 19:37:40 +0530

    Update tempcon.py

--- tempcon.py
+++ tempcon.py
@@ -1,3 +1,3 @@
-print(" The Basic Temperature Units are : \n 1. Celcius \n 2. F \n 3. Kelvin \n 4. R \n 5. \n  6. \n 7. \n 8.")
+print(" The Basic Temperature Units are : \n 1. Celcius \n 2. F \n 3. Kelvin \n 4. R \n 5. \n 6. \n 7. \n 8.")
 usertemp = int(input("Enter a Temperature Value: "))
 userunit = input("Enter the unit for the Temperature Value:- C,F,K,R,D,N,RE,RO : ")
