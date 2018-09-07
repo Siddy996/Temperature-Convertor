616ff7b7757d7374020ac520cd06251b79a0c7fe
Siddy996 <30317417+Siddy996@users.noreply.github.com>
2018-09-07 14:11:38 +0530

    Update tempcon.py

--- tempcon.py
+++ tempcon.py
@@ -1,3 +1,202 @@
-print(" The Basic Temperature Units are : \n 1. Celcius \n 2. F \n 3. Kelvin \n 4. R \n 5. \n 6. \n 7. \n 8.")
+print("Convert Temperature into Different Units\n")
+print("The Basic Temperature Units are : \n 1. Celsius \n 2. Fahrenheit \n 3. Kelvin \n 4. Rankine \n 5. Delisle \n 6. Newton\n 7. Réaumur\n 8. Rømer")
 usertemp = int(input("Enter a Temperature Value: "))
-userunit = input("Enter the unit for the Temperature Value:- C,F,K,R,D,N,RE,RO : ")
+print("Enter the Temperature Unit")
+userunit = int(input("Enter the corresponding number from 1-8: "))
+print("Enter the Unit it is to be converted into")
+finalunit= int(input("Enter the corresponding number from 1-8: "))
+
+def cf():
+	finaltemp= usertemp * 9/5 +32
+	return(finaltemp)
+	
+#def ck():
+#def cr():
+#def cd():
+#def cn():
+#def cre():
+#def cro():	
+
+while userunit==1:
+	if finalunit==2:
+		print("The Temperature in Fahrenheit is : ", cf())
+		break
+	elif finalunit==3:
+		print()
+		break
+	elif finalunit==4:
+		print()
+		break
+	elif finalunit==5:
+		print()
+		break
+	elif finalunit==6:
+		print()
+		break
+	elif finalunit==7:
+		print()
+		break
+	elif finalunit==8:
+		print()
+		break
+
+while userunit==2:
+	if finalunit==1:
+		print("The Temperature in Fahrenheit is : ", cf())
+		break
+	elif finalunit==3:
+		print()
+		break
+	elif finalunit==4:
+		print()
+		break
+	elif finalunit==5:
+		print()
+		break
+	elif finalunit==6:
+		print()
+		break
+	elif finalunit==7:
+		print()
+		break
+	elif finalunit==8:
+		print()
+		break
+
+while userunit==3:
+	if finalunit==1:
+		print("The Temperature in Fahrenheit is : ", cf())
+		break
+	elif finalunit==2:
+		print()
+		break
+	elif finalunit==4:
+		print()
+		break
+	elif finalunit==5:
+		print()
+		break
+	elif finalunit==6:
+		print()
+		break
+	elif finalunit==7:
+		print()
+		break
+	elif finalunit==8:
+		print()
+		break
+
+while userunit==4:
+	if finalunit==1:
+		print("The Temperature in Fahrenheit is : ", cf())
+		break
+	elif finalunit==2:
+		print()
+		break
+	elif finalunit==3:
+		print()
+		break
+	elif finalunit==5:
+		print()
+		break
+	elif finalunit==6:
+		print()
+		break
+	elif finalunit==7:
+		print()
+		break
+	elif finalunit==8:
+		print()
+		break
+
+while userunit==5:
+	if finalunit==1:
+		print("The Temperature in Fahrenheit is : ", cf())
+		break
+	elif finalunit==2:
+		print()
+		break
+	elif finalunit==3:
+		print()
+		break
+	elif finalunit==4:
+		print()
+		break
+	elif finalunit==6:
+		print()
+		break
+	elif finalunit==7:
+		print()
+		break
+	elif finalunit==8:
+		print()
+		break
+
+while userunit==6:
+	if finalunit==1:
+		print("The Temperature in Fahrenheit is : ", cf())
+		break
+	elif finalunit==2:
+		print()
+		break
+	elif finalunit==3:
+		print()
+		break
+	elif finalunit==4:
+		print()
+		break
+	elif finalunit==5:
+		print()
+		break
+	elif finalunit==7:
+		print()
+		break
+	elif finalunit==8:
+		print()
+		break
+
+while userunit==7:
+	if finalunit==1:
+		print("The Temperature in Fahrenheit is : ", cf())
+		break
+	elif finalunit==2:
+		print()
+		break
+	elif finalunit==3:
+		print()
+		break
+	elif finalunit==4:
+		print()
+		break
+	elif finalunit==5:
+		print()
+		break
+	elif finalunit==6:
+		print()
+		break
+	elif finalunit==8:
+		print()
+		break
+
+while userunit==8:
+	if finalunit==1:
+		print("The Temperature in Fahrenheit is : ", cf())
+		break
+	elif finalunit==2:
+		print()
+		break
+	elif finalunit==3:
+		print()
+		break
+	elif finalunit==4:
+		print()
+		break
+	elif finalunit==5:
+		print()
+		break
+	elif finalunit==6:
+		print()
+		break
+	elif finalunit==7:
+		print()
+		break
